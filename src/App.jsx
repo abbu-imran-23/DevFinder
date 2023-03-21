8681110c24f1938bcbc5e8860096a9f43459a9a1
Imran <abbuimran02@gmail.com>
2023-03-21 12:59:03 +0530

    Project Setup done!

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold font-mono text-center text-green-500'>Project Setup done!</h1>
+    </div>
+  )
+}
+
+export default App
